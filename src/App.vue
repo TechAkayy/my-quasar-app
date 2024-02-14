61eb5faee2c616456694b54f61460582a3fa6ebb
Akayy <techakayy@gmail.com>
2024-02-15 10:32:05 +1100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import { useQuasar } from 'quasar'
+
+  import 'uno.css'
+
+  // Set brand
+  const { setTheme } = useTheme()
+  setTheme()
+
+  const $q = useQuasar()
+  $q.iconMapFn = (iconName) => {
+    return {
+      icon: `i-${iconName}`,
+    }
+  }
+</script>
+
+<template>
+  <div class="q-pl-lg">
+    <!-- <router-view /> -->
+    <q-icon name="i-mdi-menu"></q-icon>
+  </div>
+</template>
